2b7e60d930bd7d2f2817afe3a57f15242e2132ef
AvilMey <61653991+AvilMey@users.noreply.github.com>
2020-03-01 13:42:56 +0100

    Add files via upload

---
+++ Boss.js
@@ -0,0 +1,12 @@
+class Boss extends Opponent {
+
+	constructor(game){
+	super(game);
+	this.speed = 2*this.speed;
+	this.myImage.src = BOSS_PICTURE;
+	this.myImageDead = BOSS_PICTURE_DEAD;
+	
+	}
+
+
+}
